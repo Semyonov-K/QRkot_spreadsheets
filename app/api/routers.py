15cb331643c5ee07abe98a05e1680c717bfe4a68
unknown <fixer20112@mail.com>
2023-05-26 22:30:33 +0300

    first try

---
+++ app/api/routers.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+from app.api.endpoints import (
+    google_api_router, charity_projects_router, donations_router, user_router
+)
+
+main_router = APIRouter()
+main_router.include_router(
+    charity_projects_router, prefix='/charity_project',
+    tags=['charity_projects']
+)
+main_router.include_router(
+    donations_router, prefix='/donation', tags=['donations']
+)
+main_router.include_router(
+    google_api_router, prefix='/google', tags=['Google']
+)
+main_router.include_router(user_router)
